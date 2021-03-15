16d6a7827f44f3b8e7d9d136be17b18f2ea8dc2d
Sanrocks07 <sanchitpillai@gmail.com>
2021-03-15 17:20:08 +0530

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://mighty-earth-03755.herokuapp.com",
+  uri: "https://mighty-earth-03755.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
